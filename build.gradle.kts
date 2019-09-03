0082cc9b9a1f35c0caf477aec2d1d9b4a24c8da9
Marais <piemarais@gmail.com>
2019-09-03 13:21:30 +0200

    [0.0.1][ISSUE#1] Add dependancy for MariaDB and JPA

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,9 +17,11 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.mariadb.jdbc:mariadb-java-client:2.1.2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
